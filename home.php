f1916e2aa5751cb79f4e3671ab0fad190f195348
amineseaghi <amineseaghi@gmail.com>
2026-02-13 22:06:14 +0100

    add update-profile

--- home.php
+++ home.php
@@ -1,13 +1,20 @@
 <?php 
 
-@include 'config.php';
+include 'config.php';
 session_start();
 $user_id = $_SESSION['user_id'];
 
 if(!isset($user_id)){
     $user_id = $_SESSION['user_id'];
     header('location:login.php');
+};
+
+if(isset($_GET['logout'])){
+    unset($user_id);
+    session_destroy();
+    header('location: login.php');
 }
+
 
 ?>
 
@@ -16,17 +23,16 @@ if(!isset($user_id)){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
     <title>home</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     
-    <div class="containers">
+    <div class="container">
         <div class="profile">
             <?php
                 $select = mysqli_query($conn, "SELECT * FROM `user_form` WHERE id = '$user_id'") or 
                 die('query failed !');
-                
                 if(mysqli_num_rows($select) > 0){
                     $fetch = mysqli_fetch_assoc($select);
                 }
@@ -39,7 +45,7 @@ if(!isset($user_id)){
 
             ?>
             <h3><?php echo $fetch['name']; ?></h3>
-            <a href="update-profile.php" class="btn">Update profile</a>
+            <a href="update-profile.php" class="btn">update profile</a> 
             <a href="home.php?logout=<?php echo $user_id; ?>" class="delete-btn">logout</a>
             <p>new <a href="login.php">login</a> or <a href="register.php">register</a></p>
         </div>
